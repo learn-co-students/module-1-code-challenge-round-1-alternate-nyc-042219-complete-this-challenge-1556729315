b0766cfe46d1e3aead796a39fc8e343c62601830
Carly Pecora <carly@carlypecora.com>
2019-05-01 13:36:15 -0400

    Done.

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,39 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
+
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
+  end
 
+  def self.all 
+  	@@all
+  end
+
+  def articles_in_magazine
+  	Article.all.select {|article| article.magazine == self }
+  end
+
+  def article_titles
+  	articles_in_magazine.map {|article| article.title }
+  end
+
+  def contributors
+	articles_in_magazine.map { |article| article.author }
+  end
+
+  def word_count
+  	total = 0
+  	articles_in_magazine.each { |article| total += article.content.split.length }
+  	total
+  end
+
+  def self.find_by_name(name)
+  	self.all.find {|magazine| magazine.name == name}
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -4,15 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
-
 ###  WRITE YOUR TEST CODE HERE ###
 
+dost = Author.new("Dostoevsky")
+maugham = Author.new("Somerset Maugham")
+seuss = Author.new("Dr. Seuss")
 
+times = Magazine.new("Times", "news")
+lit = Magazine.new("Cool Lit", "literature")
+cooler_lit = Magazine.new("Our Lit is Cooler", "literature")
 
-
-
-
-
+dost_lit = Article.new(dost, lit, "Russian Lit", "Russian Lit is cool.")
+maugham_times = Article.new(maugham, times, "English News", "Stuff happening in England.")
+dost_times = Article.new(dost, times, "News Headline", "News Content")
+dost_cooler_lit = Article.new(dost, cooler_lit, "More Russian Lit", "I wrote a book called Brothers Karamazov.")
 
 
 ### DO NOT REMOVE THIS
